a907d8ac7b698ab18b39c3306104ea995dac0b47
Dennis Buchwald <buchwald95@icloud.com>
2023-05-10 13:26:16 +0200

    api

--- src/pages/api/weather.js
+++ src/pages/api/weather.js
@@ -4,7 +4,8 @@ import axios from "axios";
 
 export default async function handler(req, res) {
   const { location } = req.query;
-  const apiKey = process.env.OPENWEATHERMAP_API_KEY;
+  const apiKey = "2ff55e360ced2b4e3e835fbc697cd597";
+  //   const apiKey = process.env.OPENWEATHERMAP_API_KEY;
 
   try {
     const response = await axios.get(
