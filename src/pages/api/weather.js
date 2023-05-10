5ed6adf81adb27c05044e7b92c0d68ee84e4a88b
Dennis Buchwald <buchwald95@icloud.com>
2023-05-10 13:21:18 +0200

    api

--- src/pages/api/weather.js
+++ src/pages/api/weather.js
@@ -1,15 +1,17 @@
 import axios from "axios";
 
-export default async function handler(req, res) {
-	const { location } = req.query;
-	const apiKey = process.env.OPENWEATHERMAP_API_KEY;
+//test
 
-	try {
-		const response = await axios.get(
-			`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}&units=metric`
-		);
-		res.status(200).json(response.data);
-	} catch (error) {
-		res.status(500).json({ error: "Failed to fetch weather data" });
-	}
+export default async function handler(req, res) {
+  const { location } = req.query;
+  const apiKey = process.env.OPENWEATHERMAP_API_KEY;
+
+  try {
+    const response = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}&units=metric`
+    );
+    res.status(200).json(response.data);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch weather data" });
+  }
 }
